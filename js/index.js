2826cfe85f2bbe6e196e025e8887e46b74861924
Max Datsun <unrusso@live.com>
2017-05-12 01:13:19 +0200

    Users displayed properly
    Fixed offline user display.
    Tabs not working as expected.

--- js/index.js
+++ js/index.js
@@ -1,54 +1,74 @@
 $(document).ready(function(){
 
+
 	var streamers = [ "freecodecamp", "llstylez", "ESL_SC2", "OgamingSC2", "dyrus",
 						"storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin",
 						"comster404"];
 
 
-	function isOnline(online){
-		if(online){
-			return "online";
-		}
-		else{
-			return "offline";
-		}
-	}
 
 	function createPanel(game, status, channelName, channelUrl, online, logo,){
-		if(isOnline(online)){
-			$(".streamers").append('<div class="panel panel-default online">'+
-									'<div class="panel-heading">' +
-									'<img class="img-resposnive img "src="' + logo + '">' +
-									'<h3><a href="' + channelUrl+ '">' + channelName +'</a></h3>' +
-									'</div> <!-- panel heading -->'+
-									'<div class="panel-body">'+
-									'<h4>Is playing: ' + game + '</h4></br>'+
-									'<h5>' + status + '<h5>'+
-									'</div>'+
-									'</div>');
+		$(".streamers").append('<div role="tabpanel" class="tab-pane active panel panel-default" id="online">'+
+						'<div class="panel-heading">' +
+						'<img class="img-resposnive img "src="' + logo + '">' +
+						'<h3><a href="' + channelUrl+ '">' + channelName +'</a></h3>' +
+						'</div> <!-- panel heading -->'+
+						'<div class="panel-body">'+
+						'<h3><strong>Is playing: </strong>' + game + '</h3></br>'+
+						'<h4>' + status + '<h4>'+
+						'</div>'+
+						'</div>');
+}
+
+	function createOfflinePanel(streamer){
+
+		$.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + streamer+ "?callback=?", function(data){
+			var channelStatus = data.status;
+			if(channelStatus == 422){
+				console.log(channelStatus);
+				$(".streamers").append('<div role="tabpanel" class="tab-pane panel panel-default" id="">'+
+								'<div class="panel-heading">' +
+								'<img class="img-resposnive img "src="' + img + '">' +
+								'<h3><a href="https://www.twitch.tv/' + streamer +'">'+streamer +'</a></h3>' +
+								'</div>'+
+								'<div class="panel-body">'+
+								'<h3>This channel is not available.</h3></br>'+
+								'<small>Error: ' + channelStatus + '</small></div></div>'
+								);
 			}
+			else if(channelStatus == 404){
+				$(".streamers").append('<div role="tabpanel" class="tab-pane panel panel-default" id="">'+
+								'<div class="panel-heading">' +
+								'<img class="img-resposnive img "src="' + img + '">' +
+								'<h3><a href="https://www.twitch.tv/' + streamer +'">'+streamer +'</a></h3>' +
+								'</div>'+
+								'<div class="panel-body">'+
+								'<h3>This channel does not exist :( </br>'+
+								'<small>Error: ' + channelStatus +'</small></div></div>'
+								);
+			}
+			else{
+				var img = data.logo;
+				$(".streamers").append('<div role="tabpanel" class="tab-pane panel panel-default" id="offline">'+
+								'<div class="panel-heading">' +
+								'<img class="img-resposnive img "src="' + img + '">' +
+								'<h3><a href="https://www.twitch.tv/' + streamer +'">'+streamer +'</a></h3>' +
+								'</div>'+
+								'<div class="panel-body">'+
+								'<h3>This channel is currently offline.</h3></br>	'+
+								'<h4><strong>Watch his last vods <a href="https://www.twitch.tv/' + streamer +'/videos/all">'+
+								'here</strong>.</a></h3></div></div>'
+								);
 		}
 
-	function createOfflinePanel(streamer, _link){
-		if(_link == "https://api.twitch.tv/kraken/channels/undefined"){
-			$(".streamers").append('<div class="panel panel-default offline">'+
-								'<div class="panel-heading">' +
-								'<img class="img-resposnive img src="">' +
-								'<h3>' + streamer +'does not exist.</h3>' +
-								'</div>');
-		}
-		else{
-			$(".streamers").append('<div class="panel panel-default offline">'+
-								'<div class="panel-heading">' +
-								'<img class="img-resposnive img src="">' +
-								'<h3><a href="https://www.twitch.tv/' + streamer +'">'+streamer +'</a> is currently offline</h3>' +
-								'</div>');
-		}
-		}
+
+		});
+	}
+
+
+
+			
 		
-		function returnStreamer(index){
-			return streamers[index];
-		}
 
 
 
@@ -67,9 +87,7 @@ $(document).ready(function(){
 				createPanel(game, status, channelName, channelUrl, online, logo);
 			}
 			else{
-				var _link = data._links.channel;
-				console.log(_link);
-				createOfflinePanel(streamer, _link);
+				createOfflinePanel(streamer);
 			}
 
 
